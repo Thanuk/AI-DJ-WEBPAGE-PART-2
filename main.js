d8797bddbbdc832bd0e366c517440b5d79bf9f7c
Thanuk <86400849+Thanuk@users.noreply.github.com>
2022-03-23 19:50:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+song = "";
+song2 = "";
+
+leftWrist_x = 0;
+leftWrist_y = 0;
+
+rightWrist_x = 0;
+rightWrist_y = 0;
+
+function preload(){
+    song = loadSound("music.mp3");
+    song2 = loadSound("music2.mp3");
+}
+
+function setup(){
+    canvas = createCanvas(400, 400);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw(){
+    image(video, 0, 0, 400, 400);
+}
+
+function modelLoaded(){
+    console.log("PoseNet is initialized");
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        leftWrist_x = results[0].pose.leftWrist.x;
+        leftWrist_y = results[0].pose.leftWrist.y;
+        console.log("Left Wrist x = "+ leftWrist_x + "Left Wrist y = "+ leftWrist_y);
+        
+        rightWrist_x = results[0].pose.rightWrist.x;
+        rightWrist_y = results[0].pose.rightWrist.y;
+        console.log("Right Wrist x = "+ rightWrist_x + "Right Wrist y = "+ rightWrist_y);
+    }
+}
